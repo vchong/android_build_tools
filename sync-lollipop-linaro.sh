93a9c2dd25d87f8518289afa3dff16584aa28ba8
Yongqin Liu <yongqin.liu@linaro.org>
2015-06-06 11:24:42 +0100

    update lollipop branch to 5.1.1_r4
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- sync-lollipop-linaro.sh
+++ sync-lollipop-linaro.sh
@@ -4,6 +4,9 @@ export BASE=$(cd $(dirname $0);pwd)
 source ${BASE}/scripts-common/sync-common.sh
 
 export MIRROR="http://android.git.linaro.org/git/platform/manifest.git"
-branch="android-5.0.2_r1"
+branch="android-5.1.1_r4"
+
+LOCAL_MANIFEST="ssh://git@dev-private-git.linaro.org/linaro-art/platform/manifest.git"
+LOCAL_MANIFEST_BRANCH="lor_ice_lolly_mr1"
 
 main "$@"
