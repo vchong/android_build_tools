01067830151cc8847e2e465a66c2a6e6276ad312
Yongqin Liu <yongqin.liu@linaro.org>
2015-08-13 21:58:33 +0800

    update sync scripts to 5.1.1_r13
    Change-Id: If581903437bd74edcd0c775c0f172763f82c1266
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- sync-lollipop-local-private.sh
+++ sync-lollipop-local-private.sh
@@ -4,7 +4,7 @@ export BASE=$(cd $(dirname $0);pwd)
 source ${BASE}/scripts-common/sync-common.sh
 
 export MIRROR="/SATA3/aosp-mirror/platform/manifest.git"
-branch="android-5.1.1_r9"
+branch="android-5.1.1_r13"
 LOCAL_MANIFEST="ssh://git@dev-private-git.linaro.org/linaro-art/platform/manifest.git"
 LOCAL_MANIFEST_BRANCH="linaro-lollipop"
 
