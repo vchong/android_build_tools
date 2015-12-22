825c85515b333f957a93bdee966d1fbcba30e127
Yongqin Liu <yongqin.liu@linaro.org>
2015-12-22 11:43:27 +0000

    update to 6.0.1_r3 for low mem
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- sync-for-memory.sh
+++ sync-for-memory.sh
@@ -4,7 +4,7 @@ export BASE=$(cd $(dirname $0);pwd)
 source ${BASE}/scripts-common/sync-common.sh
 
 export MIRROR="http://android.git.linaro.org/git/platform/manifest.git"
-branch="android-6.0.0_r26"
+branch="android-6.0.1_r3"
 
 LOCAL_MANIFEST="ssh://git@dev-private-git.linaro.org/linaro-art/platform/manifest.git"
 LOCAL_MANIFEST_BRANCH="linaro-marshmallow"
