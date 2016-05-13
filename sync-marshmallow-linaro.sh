cf938aa1fa9b3abc8ee75c3da426e88780d8cd3e
Yongqin Liu <yongqin.liu@linaro.org>
2016-05-13 05:42:55 +0100

    remove hikey kernel in liuyq.xml
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- sync-marshmallow-linaro.sh
+++ sync-marshmallow-linaro.sh
@@ -4,7 +4,7 @@ export BASE=$(cd $(dirname $0);pwd)
 source ${BASE}/scripts-common/sync-common.sh
 
 export MIRROR="http://android.git.linaro.org/git/platform/manifest.git"
-branch="android-6.0.1_r16"
+branch="android-6.0.1_r43"
 
 LOCAL_MANIFEST="ssh://git@dev-private-git.linaro.org/linaro-art/platform/manifest.git"
 LOCAL_MANIFEST_BRANCH="linaro-marshmallow"
@@ -16,12 +16,40 @@ ${BASE}/sync-projects.sh  build \
                           art \
                           android-patchsets \
                           device/linaro/hikey \
+                          kernel/linaro/hisilicon \
 
-./android-patchsets/MARSHMALLOW-MLCR-PATCHSET
-./android-patchsets/hikey-m-workarounds
-./android-patchsets/juno-m-workarounds
-./android-patchsets/marshmallow-gcc5-patchset
+function apply_patch(){
+    local patch_name=$1
+    if [ -z "${patch_name}" ]; then
+        return
+    fi
 
+    if [ ! -f "./android-patchsets/${patch_name}" ]; then
+        return
+    fi
+
+    ./android-patchsets/${patch_name}
+    if [ $? -ne 0 ]; then
+        echo "Failed to run ${patch_name}"
+        exit 1
+    fi
+
+}
+
+cd android-patchsets/
+git fetch ssh://yongqin.liu@dev-private-review.linaro.org:29418/android/android-patchsets refs/changes/01/701/1 && git cherry-pick FETCH_HEAD
+cd ..
+
+apply_patch MARSHMALLOW-MLCR-PATCHSET
+apply_patch juno-m-workarounds
+apply_patch marshmallow-gcc5-patchset
+apply_patch hikey-m-workarounds
+
+apply_patch hikey-optee
+apply_patch hikey-optee-kernel-4.4
+#apply_patch nexus9-workarounds
+
+apply_patch get-hikey-blobs
 cd device/linaro/build
 git fetch ssh://yongqin.liu@android-review.linaro.org:29418/device/linaro/common refs/changes/39/16439/1 && git cherry-pick FETCH_HEAD
 cd ../../../
