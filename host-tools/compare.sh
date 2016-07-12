f96c9782994adeabd298a041941b71296e5b1b81
Yongqin Liu <yongqin.liu@linaro.org>
2016-07-12 11:50:31 +0100

    compare.sh: used for compare difference between 2 tags
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

---
+++ host-tools/compare.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+dir_aosp_master="/home/yongqin.liu/aosp-master"
+function change_log(){
+	export old_tag="$1"
+	export new_tag="$2"
+	if [ -z "${old_tag}" ] || [ -z "${new_tag}" ]; then
+		return
+	fi
+	changelog_file="ChangeLog-${old_tag}-${new_tag}-$(date +%Y-%m-%d-%H-%M-%S).txt"
+	cd ${dir_aosp_master}
+	repo sync -j4
+	repo forall -c ' \
+	        diff_commits=$(git log --oneline --no-merges ${old_tag}..${new_tag} 2>/dev/null|wc -l)
+	        if [ ${diff_commits} -gt 0 ]; then \
+		    echo ========${REPO_PROJECT} between ${old_tag}..${new_tag}=========
+                    git diff --stat ${old_tag} ${new_tag}
+                    git log --oneline --no-merges ${old_tag}..${new_tag} 2>/dev/null
+		    echo ""
+		fi;
+               '|tee ${changelog_file}
+
+	total_line=$(grep -e 'file changed' -e "files changed" ${changelog_file}|awk 'BEGIN { file_changed=0; insertions=0; deletions=0} { file_changed=file_changed+$1; insertions=insertions+$4; deletions=deletions+$6} END { printf("%d file changed, %d insertions(+), %d deletions(-)\n", file_changed, insertions, deletions)}')
+	echo "*******************************************************"
+	echo "*******************************************************"
+	echo "    ${total_line}"
+	echo "*******************************************************"
+	echo "*******************************************************"
+	#scp ${changelog_file} people:/home/yongqin.liu/public_html/ChangeLogs
+}
+
+change_log android-6.0.1_r25 android-6.0.1_r26
+change_log android-6.0.1_r25 android-6.0.1_r45
+change_log android-6.0.1_r26 android-6.0.1_r45
+change_log android-6.0.1_r45 android-6.0.1_r46
