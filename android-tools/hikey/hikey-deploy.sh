d526d8463ff23e08171d7eebc48642baadff0e83
Yongqin Liu <yongqin.liu@linaro.org>
2016-08-11 07:21:11 +0800

    add some doc links
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- android-tools/hikey/hikey-deploy.sh
+++ android-tools/hikey/hikey-deploy.sh
@@ -9,8 +9,8 @@ if ! [ -d ${img_dir} ]; then
     echo "The specified path is not a directory:${img_dir}"
     exit 1
 fi
-#fastboot flash -u boot ${img_dir}/boot_fat.uefi.img
-fastboot flash -u boot ${img_dir}/boot.img
+fastboot flash -u boot ${img_dir}/boot_fat.uefi.img
+#fastboot flash -u boot ${img_dir}/boot.img
 fastboot flash -u system ${img_dir}/system.img
 fastboot flash -u cache ${img_dir}/cache.img
 fastboot flash -u userdata ${img_dir}/userdata.img
