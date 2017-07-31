3e965239348dd18c60b423d664a1b457d040a2d0
Yongqin Liu <yongqin.liu@linaro.org>
2017-07-31 10:37:10 +0800

    update android-tools/adbd_usb.txt
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- sync-master.sh
+++ sync-master.sh
@@ -37,11 +37,11 @@ ${BASE}/sync-projects.sh  \
 #                        system/vold \
 #                        system/core \
 
-${BASE}/sync-projects.sh \
-                        external/libdrm \
-                        device/ti/am57xevm \
-                        kernel/ti/x15/ \
-                        ti/u-boot/ \
+#${BASE}/sync-projects.sh \
+#                        external/libdrm \
+#                        device/ti/am57xevm \
+#                        kernel/ti/x15/ \
+#                        ti/u-boot/ \
 
 #                          art \
 #                          external/opencv-upstream \
@@ -50,14 +50,14 @@ fi
 #export http_proxy=192.168.0.102:37586
 #export https_proxy=192.168.0.102:37586
 
-func_apply_patch MASTER-MLCR-PATCHSET
-func_apply_patch optee-master-workarounds
+#func_apply_patch MASTER-MLCR-PATCHSET
+#func_apply_patch optee-master-workarounds
 func_apply_patch hikey-o-workarounds
-func_apply_patch hikey-optee-master
-func_apply_patch hikey-optee-4.9
-func_apply_patch hikey-clang-4.9
-func_apply_patch MASTER-CLANG-PATCHSET
-func_apply_patch x15-o-workarounds
+#func_apply_patch hikey-optee-master
+#func_apply_patch hikey-optee-4.9
+#func_apply_patch hikey-clang-4.9
+#func_apply_patch MASTER-CLANG-PATCHSET
+#func_apply_patch x15-o-workarounds
 #func_apply_patch NOUGAT-BOOTTIME-OPTIMIZATIONS-HIKEY
 #func_apply_patch NOUGAT-BOOTTIME-OPTIMIZATIONS-X15
 #func_apply_patch NOUGAT-BOOTTIME-OPTIMIZATIONS-JUNO
