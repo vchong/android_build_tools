752c721646fdbdc20ba7ebe7365d56c8588d4d0d
Yongqin Liu <yongqin.liu@linaro.org>
2017-11-24 22:12:00 +0800

    add script for vpn router and iptables
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- host-tools/iptables.sh
+++ host-tools/iptables.sh
@@ -10,40 +10,43 @@ sudo sysctl -w net.ipv4.ip_forward=1
 #sudo iptables -A FORWARD -i eth0 -o ppp0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 #sudo iptables -A FORWARD -i ppp0 -o eth0 -j ACCEPT
 
+out_inf="eno1"
+out_inf="tun0"
+local_inf="eno4"
 
 sudo iptables -F
-sudo iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-sudo iptables -A FORWARD -o tun0 -j ACCEPT
-sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -i tun0 -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -o ${out_inf} -j MASQUERADE
+#sudo iptables -A FORWARD -o ${out_inf}  -j ACCEPT
+#sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -i ${out_inf} -j ACCEPT
 
 sudo iptables -t nat -F
-sudo iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o eno1 -j MASQUERADE
-sudo iptables -t nat -A POSTROUTING -o eno0 -j MASQUERADE
-sudo iptables -A FORWARD -s 192.168.10.0/24 -o eno1 -j ACCEPT
-sudo iptables -A FORWARD -d 192.168.10.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -i eno1 -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o ${out_inf} -j MASQUERADE
+#sudo iptables -t nat -A POSTROUTING -o ${out_inf} -j MASQUERADE
+sudo iptables -A FORWARD -s 192.168.10.0/24 -o ${out_inf} -j ACCEPT
+sudo iptables -A FORWARD -d 192.168.10.0/24 -m conntrack --ctstate ESTABLISHED,RELATED -i ${out_inf} -j ACCEPT
 
 
 # Default policy to drop all incoming packets
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
+#sudo iptables -P INPUT DROP
+#sudo iptables -P FORWARD DROP
 
 # Accept incoming packets from localhost and the LAN interface
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -i eno4 -j ACCEPT
+#sudo iptables -A INPUT -i lo -j ACCEPT
+#sudo iptables -A INPUT -i ${local_inf} -j ACCEPT
 
 # Accept incoming packets from the WAN if the router initiated
 # the connection
-iptables -A INPUT -i eno0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+#sudo iptables -A INPUT -i ${out_inf} -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # Forward LAN packets to the WAN
-iptables -A FORWARD -i eno4 -o eno0 -j ACCEPT
+#sudo iptables -A FORWARD -i ${local_inf} -o ${out_inf} -j ACCEPT
 
 # Forward WAN packets to the LAN if the LAN initiated the
 # connection
-iptables -A FORWARD -i eno0 -o eno4 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+#sudo iptables -A FORWARD -i ${out_inf} -o ${local_inf} -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # NAT traffic going out the WAN interface
-iptables -t nat -A POSTROUTING -o eno0 -j MASQUERADE
+#sudo iptables -t nat -A POSTROUTING -o ${out_inf} -j MASQUERADE
 
 # rc.local needs to exit with 0
 exit 0
