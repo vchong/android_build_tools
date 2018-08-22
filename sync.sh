vchong/android_build_tools

commit a5818196479948a10526d1f1b0d5d85a604b933c

adjust application of optee patches

sync.sh
@@ -124,9 +124,9 @@ for i in ${PATCHSETS}; do
 	func_apply_patch $i
 done
 
-# if master then optee-master-workarounds will be applied automatically
-# above so no need to do it manually here
-if [ "$version" != "master" ]; then
+# if master or P then optee-master-workarounds will be applied
+# automatically above so no need to do it manually here
+if [ "$version" != "master" ] || [ "$version" != "p" ]; then
 	echo ""
 	echo ""
 	echo "applying patchset: optee-master-workarounds"

scripts-common/sync-common.sh
@@ -129,7 +129,7 @@ function func_apply_patch(){
     fi
 
     if [[ "${patch_name}" = "optee"* ]] && [[ $1 =~ [0-9]+ ]]; then
-	echo "Skipping versioned optee patches since we're using master!"
+	echo "Skip ${patch_name} since we're using master!"
 	return
     fi
 

commit 4d1661354f0921fa669c2b658d23d3b9efc63291

sync.sh: fix application of optee-master-workarounds
bug introduced in
a5818196 ("adjust application of optee patches")
where optee-master-workarounds can be applied 2x

sync.sh
@@ -126,7 +126,7 @@ done
 
 # if master or P then optee-master-workarounds will be applied
 # automatically above so no need to do it manually here
-if [ "$version" != "master" ] || [ "$version" != "p" ]; then
+if [ "$version" == "o" ] || [ "$version" == "n" ]; then
 	echo ""
 	echo ""
 	echo "applying patchset: optee-master-workarounds"